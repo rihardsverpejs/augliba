13702069dd2d0ce72b7f67c53b4bc77c4d5ed55a
rihardsverpejs <rihardsverpejs@gmail.com>
2023-05-17 10:21:32 +0300

    done

---
+++ funkcijas.js
@@ -0,0 +1,27 @@
+function mainir(){
+    document.getElementById("produkts1").src="images/kokred.jpg"
+}
+function mainiz(){
+    document.getElementById("produkts1").src="images/kokblue.jpg"
+}
+function mainib(){
+    document.getElementById("produkts1").src="images/kok.jpg"
+}
+function mainir1(){
+    document.getElementById("produkts2").src="images/ziepesr.jpg"
+}
+function mainiz1(){
+    document.getElementById("produkts2").src="images/ziepesb.jpg"
+}
+function mainib1(){
+    document.getElementById("produkts2").src="images/ziepes.jpg"
+}
+function mainir2(){
+    document.getElementById("produkts3").src="images/kreslsr.jpg"
+}
+function mainiz2(){
+    document.getElementById("produkts3").src="images/kreslsz.jpg"
+}
+function mainib2(){
+    document.getElementById("produkts3").src="images/kresls.jpg"
+}
